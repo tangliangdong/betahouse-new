e20900f9fff4770895eb0810ec19883d154398ed
xiaotang <tld@betahouse.us>
2016-07-22 15:42:39 +0800

    对page2动画的优化，但自适应还未完成，特别是手机端的。

--- js/index.js
+++ js/index.js
@@ -10,9 +10,13 @@ var fullscreen = (function(){
 		isHover = false,
 
 		newImg,
+		changeImg = $('.page2 .changediv .changeImg'),
 		hasOldImg = false,
-		imgTimeout;
-
+		oldImg,
+		imgTimeout,
+		wordIndex = 0,
+		mark = $('.changediv .mark');
+	// 初始化方法
 	function init(config){
 		initAnimate();
 		initEvent();
@@ -20,60 +24,55 @@ var fullscreen = (function(){
 	}
 
 	function imgWall(){
-		var images = $('.beta_do img'),
-			changeImg = $('.changeImg');
+		var images = $('.beta_do img');
 
 		images.hover(function() {
 			isHover = true;
 			newImg = $(this);
-			if(hasOldImg){
+			clearTimeout(imgTimeout);
+			// 不是第一次hover，且这次hover的图片和前一次不同
+			if(hasOldImg && (oldImg == undefined || newImg.attr('src')!==oldImg.attr('src'))){
 				changeImg.animate({
 					'opacity': 0,
 					},
 					500, function() {
-					changeBigImg();
+						changeBigImg();
 				});
-			}else{
+			// 第一次hover
+			}else if(!hasOldImg){
 				hasOldImg = true;
+				changeImg.animate({
+					'opacity': 0,
+					},
+					500, function() {
+						changeBigImg();
+				});
+			// 在同一张图片上hover
+			}else{
 				changeBigImg();
 			}
+			oldImg = $(this);
 		}, function() {
-			/*clearTimeout(imgTimeout);
-			if(!isHover){
-				imgTimeout = setTimeout(function(){
-					changeImg.animate({
-						'opacity': 0,
-						},
-						300, function() {
-							
-					});
-				},700);
-			}else{
-				isHover = false;
-			}*/
-			
-			
+			clearTimeout(imgTimeout);
+			imgTimeout = setTimeout('fadeOutImg()',3000);
 		});
 
+		// 更换page2右侧大图片以及下方说明文字
 		function changeBigImg(){
-			changeImg.attr({
-				"src": newImg.attr("src")
-			});
-			changeImg.css({
-				"display": 'block'
-			});
+			changeImg.attr({"src": newImg.attr("src")});
+			changeImg.css({"display": 'block'});
 			changeImg.animate({
 				'opacity': 1,
 				},
 				500, function() {
-					
 			});
-		}	
+			// 增加图片下方的说明文字
+			mark.text(newImg.attr('data'));
+			mark.addClass('animated fadeInUp');
+			mark.removeClass('fadeOutDown');
+		}
 	}
-
-
-
-
+	// page2页面初始动画
 	function initAnimate(){
 		var title = $('.betahouse_name'),
 			logo = $('.logo'),
@@ -101,7 +100,7 @@ var fullscreen = (function(){
 		},600);
 		
 	}
-
+	// page2绑定事件
 	function initEvent(){
 		$navPrev.on('click', function(event) {
 			navigate('prev');
@@ -109,13 +108,13 @@ var fullscreen = (function(){
 		$navNext.on('click', function(event) {
 			navigate('next');
 		});
-
-
 	}
-
+	// 切换屏幕内容
 	function navigate(describe){
-		var $oldItem = $items.eq(current);
-
+		var $oldItem = $items.eq(current),
+			img_li = $('.page2 .beta_do li'),
+			words = $('.changediv em'),
+			wordlength = words.length;
 		switch(describe){
 			case 'prev':
 			current = current==0 ? current = $itemcounts-1 : --current;
@@ -133,7 +132,31 @@ var fullscreen = (function(){
 			'opacity': 1,
 			'z-index': 2
 		});
+		changeImg.animate({'opacity':0}, 500,function(){
+			changeImg.css('display', 'none');
+		});
+		if(current===1){
+			img_li.addClass('animated bounceInRight');
+			wordfall(wordIndex);
+		}
 	}
 	return { init : init };
 
 })();
+// 左侧图片一段时间后消失
+function fadeOutImg(){
+	$('.changeImg').animate({'opacity':0}, 500);
+
+	$('.changediv .mark').addClass('fadeOutDown').removeClass('fadeInUp');
+}
+
+function wordfall(wordIndex){
+	var words = $('.changediv em');
+	words.eq(wordIndex).css('opacity', '1');
+	words.eq(wordIndex).addClass('animated worddown');
+	wordIndex++;
+	if(wordIndex < words.length){
+		setTimeout('wordfall('+wordIndex+')',300);
+	}
+	
+}
